bac68019991b1ccf0f602798bfd84aaca6a5db40
RaiNote <Timothy.Lutz@gmx.de>
2021-10-19 10:18:06 +0200

    Numerik partially done

---
+++ Projects/Numerik/main.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+// A1 a)
+int isValidNumber(double a, double z, double n) {
+    return (pow(a, n) < z) && (pow(a + 1, n) > z);
+}
+
+double getValidNumber(double z, double n) {
+    double a = 0;
+    while (isValidNumber(a, z, n) == 0) {
+        //printf("%lf\n", a);
+        a += PRECISION_STEP_SIZE;
+        // break if unable to find any "a"
+        //sane variant
+        if (a >= SANITY_VALUE_FOR_A)
+            return UNABLE_TO_FIND_A;
+        //insane variant
+        if (a >= DBL_MAX)
+            return UNABLE_TO_FIND_A;
+    }
+    return a;
+}
+
+//A1 b)
+double getRoot(double z, double n) {
+    double a = getValidNumber(z, n);
+    double b = a + 1;
+
+    double result = 0;
+    int counter = 0;
+
+    while (counter <= PRECISION_SANITY_LIMIT) {
+        double m = a + (b - a) / 2;
+        double tempresult = pow(m, n);
+
+        if (tempresult < z) {
+            a = m;
+        } else {
+            b = m;
+        }
+        //printf("%lf\n", result);
+
+        if (z - result <= PRECISION && z - tempresult <= PRECISION)
+            return m;
+
+        result = pow(a, n);
+        counter++;
+
+    }
+    return PRECISION_ERROR;
+}
+
+double function(double x) {
+    return pow(x, 3) - 2 * pow(x, 2) + x - 3;
+}
+
+double integrate(double start, double end, double step) {
+
+}
+
+int main() {
+    char string[1024];
+    snprintf(string, 1024, "%lf\n", getRoot(2.0, 2.0));
+    puts(string);
+    snprintf(string, 1024, "%lf\n", getRoot(4.0, 2.0));
+    puts(string);
+    snprintf(string, 1024, "%lf\n", getRoot(8.0, 3.0));
+    puts(string);
+    snprintf(string, 1024, "%lf\n", getRoot(16.0, 4.0));
+    puts(string);
+    snprintf(string, 1024, "%lf\n", getRoot(9.0, 2.0));
+    puts(string);
+    snprintf(string, 1024, "%lf\n", getRoot(25.0, 2.0));
+    puts(string);
+    snprintf(string, 1024, "%lf\n", getRoot(100.0, 2.0));
+    puts(string);
+}
---
+++ Projects/Numerik/main.h
@@ -0,0 +1,17 @@
+//
+// Created by Rai on 10.10.2021.
+//
+#pragma once
+
+#define PRECISION 0.01
+#define UNABLE_TO_FIND_A -2
+#define SANITY_VALUE_FOR_A 10000
+#define PRECISION_ERROR -1
+#define PRECISION_SANITY_LIMIT 2500
+#define PRECISION_STEP_SIZE 0.1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
